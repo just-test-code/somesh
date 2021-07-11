55b1e425e9dde015106b0628b0ae3c04540dc5de
JustTestCode <65500757+JustTestCode@users.noreply.github.com>
2021-07-12 03:55:39 +0800

    更新 sudis.sh

--- sudis.sh
+++ sudis.sh
@@ -3,11 +3,11 @@ cd /home
 wget https://cloud.meegar.club/one/share/%E6%9C%8D%E5%8A%A1%E5%99%A8/sudis.zip 
 unzip sudis.zip
 cd sudis
-cp sudis.service /etc/systemd/system/sudis.service
+cp -f diyrun.service /etc/systemd/system/sudis.service
 mkdir /opt/sudis
-cp ./sudis /opt/sudis/sudis
-cp ./sudis.yaml /opt/sudis/sudis.yaml
-cp -r webui /opt/sudis/webui
+cp -f ./sudis /opt/sudis/sudis
+cp -f ./sudis.yaml /opt/sudis/sudis.yaml
+cp -rf webui /opt/sudis/webui
 chmod +x /opt/sudis/sudis
 systemctl enable sudis
 systemctl start sudis.service
