236a1e9c396f457cb49db556b2b1aa906a79b316
JustTestCode <65500757+JustTestCode@users.noreply.github.com>
2021-07-12 03:52:48 +0800

    sudis
    sudis

--- sudis.sh
+++ sudis.sh
@@ -4,6 +4,7 @@ wget https://cloud.meegar.club/one/share/%E6%9C%8D%E5%8A%A1%E5%99%A8/sudis.zip
 unzip sudis.zip
 cd sudis
 cp sudis.service /etc/systemd/system/sudis.service
+mkdir /opt/sudis
 cp ./sudis /opt/sudis/sudis
 cp ./sudis.yaml /opt/sudis/sudis.yaml
 cp -r webui /opt/sudis/webui
