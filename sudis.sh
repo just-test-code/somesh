e0e002b129406e9a6199684480f57a28063570df
JustTestCode <65500757+JustTestCode@users.noreply.github.com>
2021-07-15 01:16:18 +0800

    更新 sudis.sh

--- sudis.sh
+++ sudis.sh
@@ -1,13 +1,5 @@
 apt-get install wget unzip -y
-cd /home
-wget https://cloud.meegar.club/one/share/%E6%9C%8D%E5%8A%A1%E5%99%A8/sudis.zip 
-unzip sudis.zip
-cd sudis
-cp -f diyrun.service /etc/systemd/system/sudis.service
-mkdir /opt/sudis
-cp -f ./sudis /opt/sudis/sudis
-cp -f ./sudis.yaml /opt/sudis/sudis.yaml
-cp -rf webui /opt/sudis/webui
-chmod +x /opt/sudis/sudis
-systemctl enable sudis
-systemctl start sudis.service
+mkdir /opt/kcptun
+cd /opt/kcptun
+wget https://github.com/xtaci/kcptun/releases/download/v20210624/kcptun-linux-amd64-20210624.tar.gz
+tar -xzf kcptun-linux-amd64-20210624.tar.gz
